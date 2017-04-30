df80b135c5f78ade2ce8173e90be8ae385dd92bd
Taha <taha@Tahas-MacBook-Pro.local>
2017-04-30 14:29:38 +0530

    list of objects for sale

---
+++ mint.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.8;
+
+contract TokenSupply {
+    
+    address public owner;
+    uint256 public totalSupply;
+    mapping (address => uint256) public balanceOf;
+    
+    /*To notify the client*/
+    event Transfer(address indexed_to, address indexed_from, uint amount);
+    
+    function TokenSupply(uint initialSupply){
+            totalSupply = initialSupply;
+    }
+    
+    function onwed(){
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        if(msg.sender != owner)
+            throw;
+            _;
+    }
+    
+    function mintToken(address target, uint256 mintedAmount) onlyOwner {
+    
+    balanceOf[target] += mintedAmount;
+    totalSupply += mintedAmount;
+    Transfer(0, owner, mintedAmount);
+    Transfer(owner, target, mintedAmount);
+    }
+}
